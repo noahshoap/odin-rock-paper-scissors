76f1a294ec2be22df98ecce3369adde66d94f307
Noah Shoap <nshoap@vols.utk.edu>
2023-07-06 12:30:25 -0400

    finished ui version

--- game.js
+++ game.js
@@ -3,6 +3,17 @@
 // 7-5-23
 // Implements rock, paper, scissors in JS console
 
+const winNode = document.querySelector(".wins")
+const lossNode = document.querySelector(".losses")
+const tieNode = document.querySelector(".ties")
+const resultNode = document.querySelector(".result")
+
+const buttons = document.getElementsByTagName("button")
+
+for (const button of buttons) {
+    button.addEventListener('click', buttonClicked);
+}
+
 const choices = ['Rock', 'Paper', 'Scissors']
 const outcomeMap = new Map()
 
@@ -38,41 +49,45 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
+// Setup outcomes map.
+setupOutcomes()
 
-  // Setup outcomes map.
-  setupOutcomes()
+let winCount = 0
+let lossCount = 0
+let drawCount = 0
 
-  let winCount = 0
-  let lossCount = 0
-  let drawCount = 0
-
-  for (let i = 0; i < 5; ++i) {
-    let playerSelection = prompt("Rock, Paper, or Scissors?")
-    let computerSelection = getComputerChoice()
+function buttonClicked(event) {
+    let playerSelection = this.dataset.value;
+    let computerSelection = getComputerChoice();
 
     let outcome = playRound(playerSelection, computerSelection)
 
+    resultNode.classList.remove('lost');
+    resultNode.classList.remove('won');
+    resultNode.classList.remove('draw');
+
     switch (outcome) {
-      case 0:
-        console.log(`${playerSelection} ties ${computerSelection}.  Draw!`)
-        ++drawCount
-        break
-      case 1:
-        console.log(`${playerSelection} beats ${computerSelection}.  You won this round!`)
-        ++winCount
-        break
-      case 2:
-        console.log(`${computerSelection} beats ${playerSelection}.  You lost this round.`)
-        ++lossCount
-        break
-      default:
-        console.log('You broke the game.')
-        break
+        case 0:
+            resultNode.innerText = `${playerSelection} ties ${computerSelection}.  Draw!`;
+            resultNode.classList.add('draw');
+            ++drawCount
+            break
+        case 1:
+            resultNode.innerText = `${playerSelection} beats ${computerSelection}.  You won this round!`;
+            resultNode.classList.add('won');
+            ++winCount
+            break
+        case 2:
+            resultNode.innerText = `${computerSelection} beats ${playerSelection}.  You lost this round.`;
+            resultNode.classList.add('lost');
+            ++lossCount
+            break
+        default:
+            resultNode.innerText = `You broke the game.`
+            break
     }
-  }
 
-  console.log(`You finished a record of ${winCount} wins, ${lossCount} losses, and ${drawCount} draws.  Play again soon!`)
+    winNode.innerText = `Wins ${winCount}`;
+    lossNode.innerText = `Losses ${lossCount}`;
+    tieNode.innerText = `Ties ${drawCount}`;
 }
-
-game()
